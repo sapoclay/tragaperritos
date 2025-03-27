dc196e2c9a640c6ae2555d8d8874f432e8eff2aa
sapoclay <admin@entreunosyceros.net>
2025-03-27 13:28:09 +0100

    Add run_app

--- run_app.py
+++ run_app.py
@@ -12,7 +12,6 @@ def is_venv_exists():
 def create_venv():
     print("Creando el entorno virtual...")
     venv.create('venv', with_pip=True)
-    activate_venv()
 
 def get_python_executable():
     if platform.system().lower() == 'windows':
@@ -27,6 +26,9 @@ def get_pip_executable():
 def install_requirements():
     pip_exe = get_pip_executable()
     requirements_file = 'requirements.txt'
+    
+    print("Instalando setuptools para resolver dependencias...")
+    subprocess.run([pip_exe, 'install', '--upgrade', 'setuptools'], check=True)
     
     if not os.path.exists(requirements_file):
         print(f"Error: {requirements_file} not found")
@@ -46,43 +48,12 @@ def run_main_app():
     print("Iniciando la aplicación...")
     subprocess.run([python_exe, main_file], check=True)
 
-def activate_venv():
-    venv_dir = 'venv'
-    if platform.system().lower() == 'windows':
-        activate_script = os.path.join(venv_dir, 'Scripts', 'activate.bat')
-    else:
-        activate_script = os.path.join(venv_dir, 'bin', 'activate')
-
-    if not os.path.exists(activate_script):
-        print(f"Error: El script de activación del entorno virtual no ha funcionado {activate_script}")
-        sys.exit(1)
-
-    print("Activando el entorno virtual...")
-    try:
-        if platform.system().lower() == 'windows':
-            subprocess.run(['cmd', '/c', activate_script], check=True)
-        else:
-            subprocess.run(['source', activate_script], check=True)
-
-        # Activar el entorno virtual en el script
-        venv_path = os.path.abspath(venv_dir)
-        os.environ['VIRTUAL_ENV'] = venv_path
-        os.environ['PATH'] = os.path.join(venv_path, 'Scripts' if platform.system().lower() == 'windows' else 'bin') + os.pathsep + os.environ.get('PATH', '')
-        # Elimina PYTHONHOME si existe, ya que puede interferir con el entorno virtual
-        os.environ.pop('PYTHONHOME', None)
-        print("Entorno virtual activado correctamente!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error activando el entorno virtual: {e}")
-        sys.exit(1)
-
 def main():
     # Cambiar al directorio que contenga este script
     os.chdir(Path(__file__).parent)
     
     if not is_venv_exists():
         create_venv()
-    else:
-        activate_venv()
     
     try:
         install_requirements()
